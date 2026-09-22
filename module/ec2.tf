5e5c2001377f84efa0928e7bf58ce68a53a9da56
sudarshanlnx <87026085+sudarshanlnx@users.noreply.github.com>
2026-09-22 09:09:49 +0530

    Update ec2.tf

--- module/ec2.tf
+++ module/ec2.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "webserver" {
-  ami           = var.amiid
-  instance_type = var.machinetype
-  key_name      = var.keyname
+  ami             = var.amiid
+  instance_type   = var.machinetype
+  vpc_security_group_ids = [aws_security_group.global-sg-2025.id]
+  subnet_id = "subnet-0d374b517c661d6d0"
+  key_name        = var.keyname
 
   tags = {
     Name = var.mytag
