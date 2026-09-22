f2bcafb225d5392fdb639d03d1fe82e934add034
sudarshanlnx <87026085+sudarshanlnx@users.noreply.github.com>
2026-09-22 09:08:32 +0530

    Add load balancer and health check variables
    Added new variables for load balancer configuration and health checks.

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,6 +1,7 @@
 variable "sgname" {
   sensitive = true
 }
+
 variable "cidr" {}
 
 variable "mytag" {}
@@ -10,3 +11,37 @@ variable "amiid" {}
 variable "machinetype" {}
 
 variable "keyname" {}
+
+variable "lb_name" {}
+
+variable "lb_internal" {
+  default = false
+}
+
+variable "lb_subnets" {
+  type = list(string)
+}
+
+variable "target_group_name" {}
+
+variable "target_port" {
+  default = 80
+}
+
+variable "target_protocol" {
+  default = "HTTP"
+}
+
+variable "vpc_id" {}
+
+variable "health_check_path" {
+  default = "/"
+}
+
+variable "listener_port" {
+  default = 80
+}
+
+variable "listener_protocol" {
+  default = "HTTP"
+}
